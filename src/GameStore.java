6bcb7b250e0661855a0f404ce21bcf55fa7ee726
Michał Bugajski <michabugajski2@gmail.com>
2026-06-26 17:26:54 +0200

    input reader added with method that validates if input date is correct

--- src/GameStore.java
+++ src/GameStore.java
@@ -1,2 +1,55 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class GameStore {
+    public static void main(String[] args) {
+        ArrayList<Game> games = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter game identifier: ");
+            String inputIdentifier = sc.nextLine();
+
+            if (inputIdentifier.isEmpty()) {
+                break;
+            } else {
+                System.out.println("Enter game title: ");
+                String inputGameTitle = sc.nextLine();
+            }
+        }
+    }
+
+    // Validate if input date is in correct format. Date returns in new SimpleDate object.
+    public static SimpleDate readDate(Scanner sc) {
+        while (true) {
+            System.out.println("Enter release date (day.month.year)");
+            String inputDate = sc.nextLine();
+            if (!inputDate.matches("\\d{1,2}\\.\\d{1,2}\\d{4}")) {
+                System.out.println("Wrong date format. Use day.month.year, for example 24.03.2017.");
+                continue;
+            }
+
+            String[] dateParts = inputDate.split("\\.");
+            int day = Integer.valueOf(dateParts[0]);
+            int month = Integer.valueOf(dateParts[1]);
+            int year = Integer.valueOf(dateParts[2]);
+
+            if (day < 1 || day > 30) {
+                System.out.println("Wrong day. Day must be between 1 and 30.");
+                continue;
+            }
+
+            if (month < 1 || month > 12) {
+                System.out.println("Wrong month. Month must be between 1 and 12.");
+                continue;
+            }
+
+            if (year < 1 || year > 9999) {
+                System.out.println("Wrong year. Year must cannot be below 9999.");
+                continue;
+            }
+
+            return new SimpleDate(day, month, year);
+        }
+    }
 }
