89c9ec0f63bb05324cd0c1623b7aee4eb2da49f8
Michał Bugajski <michabugajski2@gmail.com>
2026-06-26 18:27:14 +0200

    game getPrice() method added.

--- src/Game.java
+++ src/Game.java
@@ -2,9 +2,9 @@ public class Game {
     private String identifier;
     private String title;
     private SimpleDate date;
-    private int price;
+    private Money price;
 
-    public Game(String identifier, String title, SimpleDate date, int price) {
+    public Game(String identifier, String title, SimpleDate date, Money price) {
         this.identifier = identifier;
         this.title = title;
         this.date = date;
@@ -29,6 +29,11 @@ public class Game {
         Game comparedGame = (Game) compared;
 
         return (this.identifier.equals(comparedGame.identifier));
+    }
+
+    // Returns this game price.
+    public Money getPrice() {
+        return this.price;
     }
 
 }
